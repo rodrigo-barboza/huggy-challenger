afbc9a155f2820095eacbdb4be3556846a66a163
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-03 22:30:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Versões utilizadas nestes projeto
+
+- PHP 8.4
+- Laravel 12
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::get('teste', fn() => 'OK');
+});
+
