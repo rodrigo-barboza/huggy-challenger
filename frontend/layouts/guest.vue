c58f297549a2c102044c9aec09c6cf2d13107713
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-05 16:34:25 -0300

    feat: adiciona guest layout

---
+++ frontend/layouts/guest.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="h-screen w-full flex flex-col justify-center items-center">
+        <main class="max-w-[930px]">
+            <slot></slot>
+        </main>
+    </div>
+</template>
