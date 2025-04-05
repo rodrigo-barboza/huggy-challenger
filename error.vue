4cd0de6825d59820e57a33145cbefbf8c37e0a3c
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-05 16:32:19 -0300

    feat: adiciona pagina default de erro do nuxt

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Página de erro</h2>
+        <p>{{ error.statusCode }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    error: {
+        type: Object,
+    },
+});
+
+const backHome = () => clearError({ redirect: '/' });
+
+</script>
