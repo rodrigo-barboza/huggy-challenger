12326e2237651319fea6fd46429d55560b4c1c2a
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-07 16:48:09 -0300

    feat: resolve os dados dos gráficos

--- frontend/components/ChartsSection.vue
+++ frontend/components/ChartsSection.vue
@@ -1,16 +1,41 @@
 <template>
+
     <section class="flex flex-col gap-[80px] px-14 py-6">
-        <div class="flex flex-col gap-8">
+        <div
+            v-for="({ title, data }, index) in insights"
+            class="flex flex-col gap-8"
+            :key="index"
+        >
             <div class="text-sm font-medium leading-[18px] tracking-[0.1px] text-[#262626]">
-                Segmentação por estado
+                {{ title }}
             </div>
-            <PieChart />
-        </div>
-        <div class="flex flex-col gap-8">
-            <div class="text-sm font-medium leading-[18px] tracking-[0.1px] text-[#262626]">
-                Segmentação por cidade
-            </div>
-            <PieChart />
+            <PieChart
+                :series="getSeries(data)"
+                :labels="getLabels(data)"
+            />
         </div>
     </section>
 </template>
+
+<script setup>
+
+const props = defineProps({
+    insights: {
+        type: Array,
+        required: true,
+    },
+});
+
+const getLabels = (data) => {
+    if (data.length === 0) return [];
+
+    return data.map(item => Object.values(item)[0]);
+};
+
+const getSeries = (data) => {
+    if (data.length === 0) return [];
+
+    return data.map(item => Object.values(item)[1]);
+};
+
+</script>
