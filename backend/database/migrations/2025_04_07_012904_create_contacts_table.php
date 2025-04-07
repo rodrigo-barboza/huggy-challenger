fc0303dfaf2b96332b2261071160f1dc3dadf6af
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-07 01:03:11 -0300

    feat: adiciona model e migration de contact

---
+++ backend/database/migrations/2025_04_07_012904_create_contacts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('address', 255);
+            $table->string('cellphone', 15);
+            $table->string('email', 255)->unique();
+            $table->string('name', 100);
+            $table->string('neighborhood', 255);
+            $table->string('phone', 15);
+            $table->string('state', 50);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
