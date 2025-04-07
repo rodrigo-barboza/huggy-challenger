96523534776fe5a6558347f0ea178465bf24b1f7
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-07 02:14:42 -0300

    feat: adiciona mutator para colocar o nome do contato title case

--- backend/app/Models/Contact.php
+++ backend/app/Models/Contact.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Contact extends Model
 {
     protected $guarded = [];
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value): string => Str::title($value),
+        );
+    }
 }
