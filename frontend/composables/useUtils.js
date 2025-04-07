2ba38b70d7ca1e7cd8bf819773d85a9ef06ec2b4
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-07 02:15:01 -0300

    fix: corrige bug de quando o nome não está definido no generate avatar

--- frontend/composables/useUtils.js
+++ frontend/composables/useUtils.js
@@ -1,41 +1,45 @@
 export default () => {
-    const hasSlots = (slots, name) => Object.keys(slots).includes(name);
+	const hasSlots = (slots, name) => Object.keys(slots).includes(name);
 
-    const sortTable = (items, field, asc) => {
-        return items.sort((a, b) => {
-            const multiplier = asc ? 1 : -1;
+	const sortTable = (items, field, asc) => {
+		return items.sort((a, b) => {
+			const multiplier = asc ? 1 : -1;
 
-            return a[field] < b[field] ? -1 * multiplier : a[field] > b[field] ? 1 * multiplier : 0;
-        });
-    }
+			return a[field] < b[field] ? -1 * multiplier : a[field] > b[field] ? 1 * multiplier : 0;
+		});
+	}
 
-    const generateAvatar = (name, size = 32) => {
-        const initials = name.split(' ').slice(0, 2)
-            .map(word => word[0]?.toUpperCase() || '')
-            .join('');
+	const generateAvatar = (name, size = 32) => {
+		if (!name) {
+			return '';
+		}
 
-        const svg = `
-          <svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="#F8F7FD"/>
-            <text 
-              x="50%" 
-              y="50%" 
-              font-family="Arial" 
-              font-size="${size * 0.4}" 
-              font-weight="bold" 
-              fill="#180D6E"
-              text-anchor="middle" 
-              dominant-baseline="middle"
-            >${initials}</text>
-          </svg>
-        `;
+		const initials = name.split(' ').slice(0, 2)
+			.map(word => word[0]?.toUpperCase() || '')
+			.join('');
 
-        return `data:image/svg+xml;base64,${btoa(svg)}`;
-    }
+		const svg = `
+					<svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" xmlns="http://www.w3.org/2000/svg">
+						<rect width="100%" height="100%" fill="#F8F7FD"/>
+						<text 
+							x="50%" 
+							y="50%" 
+							font-family="Arial" 
+							font-size="${size * 0.4}" 
+							font-weight="bold" 
+							fill="#180D6E"
+							text-anchor="middle" 
+							dominant-baseline="middle"
+						>${initials}</text>
+					</svg>
+				`;
 
-    return {
-        hasSlots,
-        sortTable,
-        generateAvatar,
-    };
+		return `data:image/svg+xml;base64,${btoa(svg)}`;
+	}
+
+	return {
+		hasSlots,
+		sortTable,
+		generateAvatar,
+	};
 };
