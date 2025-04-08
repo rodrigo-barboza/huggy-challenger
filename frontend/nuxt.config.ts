4369bc5a481d72d06646674363a3d662e5789d58
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-08 18:03:54 -0300

    refactor: organiza melhro o nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,25 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
-	components: ['~/components', '~/shared/icons'],
+	devtools: {
+		enabled: true,
+	},
+	components: [
+		'~/components',
+		'~/shared/icons',
+	],
 	app: {
 		head: {
 			link: [
 				{
 					rel: 'stylesheet',
-					href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
-				}
-			]
-		}
+					href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+				},
+			],
+		},
 	},
 	build: {
-		transpile: ['vue3-apexcharts']
+		transpile: ['vue3-apexcharts'],
 	},
 	runtimeConfig: {
 		public: {
-			apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000' // fallback para desenvolvimento
-		}
+			apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000', // fallback para desenvolvimento
+		},
 	},
 	modules: [
 		'@nuxtjs/tailwindcss',
@@ -32,8 +37,8 @@ export default defineNuxtConfig({
 		componentNames: {
 			Form: 'VeeForm',
 			Field: 'VeeField',
-			ErrorMessage: 'VeeErrorMessage'
-		}
+			ErrorMessage: 'VeeErrorMessage',
+		},
 	},
 	css: ['@/assets/css/main.css'],
 	ssr: false,
